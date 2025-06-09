ea56cdd1c5625be93797398e712250adc33f875a
Anshukumar123975 <anshupat2020@gmail.com>
2025-06-09 13:12:08 +0530

    Added the remaining concepts semantic validation models and updated the test file and updated the package.json for github actions
    Signed-off-by: Anshukumar123975 <anshupat2020@gmail.com>

--- semantic/specifications/concepts/tests/concepts.test.js
+++ semantic/specifications/concepts/tests/concepts.test.js
@@ -170,3 +170,161 @@ describe('Semantic Validation - CLASS_DECLARATION_008 - property must not duplic
         ).not.toThrow();
     });
 });
+
+ describe('DECLARATION_001 - Unique Declarations', () => {
+  it('should throw semantic error when duplicate declarations exist', () => {
+    const cto = loadCTO('semantic/specifications/concepts/models/DECLARATION_001/declaration_001_duplicate_within_model.cto');
+    const modelManager = new ModelManager();
+    modelManager.addCTOModel(cto, 'declaration_001_unique_declarations_invalid.cto', true);
+    expect(() =>
+      modelManager.validateModelFiles()
+    ).toThrow(/Class ".*" has more than one field named ".*"\./);
+  });
+
+   it('should pass when all declarations are unique', () => {
+    const cto = loadCTO('semantic/specifications/concepts/models/DECLARATION_001/declaration_001_unique_within_model.cto');
+    const modelManager = new ModelManager();
+    modelManager.addCTOModel(cto, 'declaration_001_unique_declarations_valid.cto', true);
+    expect(() =>
+      modelManager.validateModelFiles()
+    ).not.toThrow();
+  });
+});
+
+ describe('MODEL_ELEMENT_002 - Valid Identifier Name', () => {
+  it('should throw semantic error on invalid identifier names', () => {
+    const cto = loadCTO('semantic/specifications/concepts/models/MODEL_ELEMENT_002/model_element_002_invalid_identifier_name.cto');
+    const modelManager = new ModelManager();
+    expect(() =>
+        modelManager.addCTOModel(cto, 'model_element_002_invalid_identifier_name.cto', true)
+    ).toThrow();
+  });
+
+   it('should pass for valid identifier names', () => {
+    const cto = loadCTO('semantic/specifications/concepts/models/MODEL_ELEMENT_002/model_element_002_valid_identifier_name.cto');
+    const modelManager = new ModelManager();
+    modelManager.addCTOModel(cto, 'model_element_002_valid_identifier_name.cto', true);
+    expect(() =>
+      modelManager.validateModelFiles()
+    ).not.toThrow();
+  });
+});
+
+ describe('RELATIONSHIP_001 - Relationship to Non-Primitive Type', () => {
+  it('should throw semantic error when relationship is to a primitive type', () => {
+    const cto = loadCTO('semantic/specifications/concepts/models/RELATIONSHIP_001/relationship_001_non-primitive_type_relationship.cto');
+    const modelManager = new ModelManager();
+    modelManager.addCTOModel(cto, 'relationship_001_non_primitive_type_invalid.cto', true);
+    expect(() =>
+      modelManager.validateModelFiles()
+    ).toThrow(/Relationship .* cannot be to the primitive type .*/);
+  });
+
+   it('should pass when relationship is to a non-primitive type', () => {
+    const cto = loadCTO('semantic/specifications/concepts/models/RELATIONSHIP_001/relationship_001_primitive_type_relationship.cto');
+    const modelManager = new ModelManager();
+    modelManager.addCTOModel(cto, 'relationship_001_non_primitive_type.cto', true);
+    expect(() =>
+      modelManager.validateModelFiles()
+    ).not.toThrow();
+  });
+});
+
+ describe('RELATIONSHIP_002 - Relationship Type Must Exist', () => {
+  it('should throw semantic error if relationship type does not exist', () => {
+    const cto = loadCTO('semantic/specifications/concepts/models/RELATIONSHIP_002/relationship_002_type_not_exist.cto');
+    const modelManager = new ModelManager();
+    modelManager.addCTOModel(cto, 'relationship_002_existing_type_invalid.cto', true);
+    expect(() =>
+      modelManager.validateModelFiles()
+    ).toThrow(/Undeclared type .*/);
+  });
+
+   it('should pass when relationship type exists', () => {
+    const cto = loadCTO('semantic/specifications/concepts/models/RELATIONSHIP_002/relationship_002_existing_type.cto');
+    const modelManager = new ModelManager();
+    modelManager.addCTOModel(cto, 'relationship_002_existing_type.cto', true);
+    expect(() =>
+      modelManager.validateModelFiles()
+    ).not.toThrow();
+  });
+});
+
+ describe('RELATIONSHIP_003 - Relationship Must Be to Identified Type', () => {
+  it('should throw semantic error when relationship target is not identified', () => {
+    const cto = loadCTO('semantic/specifications/concepts/models/RELATIONSHIP_003/relationship_003_not_identified_type.cto');
+    const modelManager = new ModelManager();
+    modelManager.addCTOModel(cto, 'relationship_003_identified_type_invalid.cto', true);
+    expect(() =>
+      modelManager.validateModelFiles()
+    ).toThrow(/Relationship .* must be to a class that has an identifier, but this is to .*/);
+  });
+
+   it('should pass when relationship target is identified', () => {
+    const cto = loadCTO('semantic/specifications/concepts/models/RELATIONSHIP_003/relationship_003_identified_type.cto');
+    const modelManager = new ModelManager();
+    modelManager.addCTOModel(cto, 'relationship_003_identified_type.cto', true);
+    expect(() =>
+      modelManager.validateModelFiles()
+    ).not.toThrow();
+  });
+});
+
+ describe('PROPERTY_002 - Non-Primitive Property Type Must Exist', () => {
+  it('should throw semantic error if non-primitive property type does not exist', () => {
+    const cto = loadCTO('semantic/specifications/concepts/models/PROPERTY_002/property_002_type_does_not_exist.cto');
+    const modelManager = new ModelManager();
+    modelManager.addCTOModel(cto, 'property_002_existing_type_invalid.cto', true);
+    expect(() =>
+      modelManager.validateModelFiles()
+    ).toThrow(/Undeclared type ".*"/);
+  });
+
+   it('should pass when non-primitive property type exists', () => {
+    const cto = loadCTO('semantic/specifications/concepts/models/PROPERTY_002/property_002_type_exist.cto');
+    const modelManager = new ModelManager();
+    modelManager.addCTOModel(cto, 'property_002_existing_type.cto', true);
+    expect(() =>
+      modelManager.validateModelFiles()
+    ).not.toThrow();
+  });
+});
+
+ describe('PROPERTY_003 - Property Meta Type Must Exist', () => {
+  it('should throw semantic error for invalid property meta types', () => {
+    const cto = loadCTO('semantic/specifications/concepts/models/PROPERTY_003/property_003_meta_type_invalid.cto');
+    const modelManager = new ModelManager();
+    expect(() =>
+      modelManager.addCTOModel(cto, 'property_003_invalid_meta_type.cto', true)
+    ).toThrow();
+  });
+
+   it('should pass for valid property meta types', () => {
+    const cto = loadCTO('semantic/specifications/concepts/models/PROPERTY_003/property_003_meta_type_valid.cto');
+    const modelManager = new ModelManager();
+    modelManager.addCTOModel(cto, 'property_003_valid_meta_type.cto', true);
+    expect(() =>
+      modelManager.validateModelFiles()
+    ).not.toThrow();
+  });
+});
+
+ describe('DECORATED_001 - Decorator Not Applied More Than Once', () => {
+  it('should throw semantic error when decorator applied multiple times', () => {
+    const cto = loadCTO('semantic/specifications/concepts/models/DECORATED_001/decorated_001_duplicate_decorator.cto');
+    const modelManager = new ModelManager();
+    modelManager.addCTOModel(cto, 'decorated_001_multiple_decorator_invalid.cto', true);
+    expect(() =>
+      modelManager.validateModelFiles()
+    ).toThrow(/Duplicate decorator .*/);
+  });
+
+   it('should pass when decorator applied once', () => {
+    const cto = loadCTO('semantic/specifications/concepts/models/DECORATED_001/decorated_001_unique_decorator.cto');
+    const modelManager = new ModelManager();
+    modelManager.addCTOModel(cto, 'decorated_001_single_decorator.cto', true);
+    expect(() =>
+      modelManager.validateModelFiles()
+    ).not.toThrow();
+  });
+});
