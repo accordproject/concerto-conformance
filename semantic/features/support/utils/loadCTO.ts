0e3c3880d865b45fa943d7bdd90ac932fdb2a32e
Anshukumar123975 <anshupat2020@gmail.com>
2025-06-13 19:26:58 +0530

    Revert "Added features folder within semantic for behaviour driven testing using cucumber specifications like maps and scalars"
    This reverts commit b15a041e830bf42a69ea6ddb16792882982ccb42.
    
    Signed-off-by: Anshukumar123975 <anshupat2020@gmail.com>

--- semantic/features/support/utils/loadCTO.ts
+++
@@ -1,7 +0,0 @@
-import fs from 'fs';
-import path from 'path';
-
-export const loadCTO = (relativePath: string): string => {
-  const basePath = path.resolve('semantic/specifications/');
-  return fs.readFileSync(path.join(basePath, relativePath), 'utf8');
-};
