905927797d1b7dc6402e91969d85ed5b5ecf8a49
Anshukumar123975 <anshupat2020@gmail.com>
2025-07-25 20:25:39 +0530

    Add validate to test the modelLoader, Factory and Serializer
    Signed-off-by: Anshukumar123975 <anshupat2020@gmail.com>

--- semantic/features/support/C#/Semantic.Support.CSharp/obj/Debug/net6.0/Semantic.Support.CSharp.AssemblyInfo.cs
+++ semantic/features/support/C#/Semantic.Support.CSharp/obj/Debug/net6.0/Semantic.Support.CSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Semantic.Support.CSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a565f9047bb3b3eb26525cedffd7946ff5b481c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2773a9b63650b8c3362771be84170f04fce5f0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Semantic.Support.CSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Semantic.Support.CSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
